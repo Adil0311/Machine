0cfe954587d2efaf1bdfc49f9fa13473c0663cbd
Adil Lagzouli <77105479+Adil0311@users.noreply.github.com>
2024-12-10 19:01:18 +0100

    Delete src directory

--- src/main/java/org/uniupo/it/components/Screen.java
+++
@@ -1,9 +0,0 @@
-package org.uniupo.it.components;
-
-public class Screen {
-
-    public void updateScreen(String message) {
-        System.out.println("Screen updated with message: " + message);
-    }
-
-}
